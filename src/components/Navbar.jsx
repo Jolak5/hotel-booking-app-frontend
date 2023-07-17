9ac468d0a2956fcc5b36ee6b568d729140081f85
Amen-Musingarimi <takudzwamusinga@gmail.com>
2023-07-17 16:09:02 +0200

    Add Navlinks toogle logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import './../styles/Navbar.css';
 import { FaBars } from 'react-icons/fa';
+import { useRef } from 'react';
 
 export default function Navbar() {
+  const navRef = useRef();
+  const showNavLinks = () => {
+    console.log('Nav Bars Clicked');
+    navRef.current.classList.toggle('nav-toggle');
+  };
   return (
     <div>
       <header className="nav-container">
-        <button className="menu-icon">
+        <button className="menu-icon" onClick={showNavLinks}>
           <FaBars />
         </button>
-        <nav className="nav-links">
+        <nav className="nav-links" ref={navRef}>
           <NavLink to="my-reservation">My Reservations</NavLink>
           <NavLink to="Add-hotel-room">Add hotel room</NavLink>
           <NavLink to="reserve">Reserve</NavLink>
