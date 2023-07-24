7b6922b80a774aef74bcc7cce002837e669a35c5
isaacg <iwanyoike65@gmail.com>
2023-07-24 21:51:40 +0300

    Style details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { FaGreaterThan } from 'react-icons/fa';
+import { useParams, Link } from 'react-router-dom';
 import { fetchHotel } from '../redux/Details/detailsSlice';
 import styles from './Details.module.css';
 
@@ -24,27 +25,30 @@ const Details = () => {
         {hoteldetails.image
         && <img className={styles.detimg} src={hoteldetails.image.url} alt={hoteldetails.name} />}
       </div>
-      <div className={styles.divdetails}>
-        <h3>{hoteldetails.name}</h3>
-        <table className={styles.detailstable}>
-          <tr className={styles.divtr}>
-            <td> Hotel price</td>
-            <td>
-              {' '}
-              {hoteldetails.price}
-              {' '}
-            </td>
-          </tr>
-          <tr className={styles.divtr}>
-            <td> Duration </td>
-            <td>
-              {' '}
-              {hoteldetails.duration}
-              {' '}
-            </td>
-          </tr>
-        </table>
-        <button className={styles.reserve} type="submit"> Reserve </button>
+      <div className={styles.hoteltextdetails}>
+        <h2 className={styles.hoteldetailsname}>{hoteldetails.name}</h2>
+        <p className={styles.hoteldetailsdescription}>{hoteldetails.description}</p>
+        <div className={styles.hotelpricecontainer}>
+          <p className={styles.textheading}>Price Per Night</p>
+          <span className="price-value">
+            $
+            {hoteldetails.price}
+          </span>
+        </div>
+        <div className={styles.durationcontainer}>
+          <p className={styles.textheading}>Duration</p>
+          <span className="duration-value">
+            {hoteldetails.duration}
+            days
+          </span>
+        </div>
+        <Link to="/hotels" className={styles.morehotelslink}>
+          Discover More Hotels
+          <FaGreaterThan />
+        </Link>
+        <Link to="/reservations/new" className={styles.reserve}>
+          Reserve
+        </Link>
       </div>
     </div>
   );
