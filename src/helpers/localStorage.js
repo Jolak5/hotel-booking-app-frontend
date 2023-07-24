2cf14f99dadf77abcfa9274d69bf34ec13e6bf0e
isaacg <iwanyoike65@gmail.com>
2023-07-24 16:40:43 +0300

    Add hotel room and redux slice

--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -2,7 +2,15 @@ export const setLocalStorage = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-export const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+export const getLocalStorage = (key) => {
+  const data = localStorage.getItem(key);
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error parsing JSON from local storage', error);
+    return null; // or return {} or any other default value
+  }
+};
 
 export const removeLocalStorage = (key) => {
   localStorage.removeItem(key);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import AuthenticationReducer from './auth/authenticationSlice';
+import homeReducer from './Home/homeSlice';
 
 const store = configureStore({
   reducer: {
     auth: AuthenticationReducer,
+    home: homeReducer,
   },
 });
 
