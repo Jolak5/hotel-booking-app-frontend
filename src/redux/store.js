40ec4ffd083ca8ba4273778bd0b2a62e83955e2b
jolak5 <kayodej4@gmail.com>
2023-07-24 10:18:45 +0100

    fix errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import roomsReducer from './rooms/roomsSlice';
+
 import AuthenticationReducer from './auth/authenticationSlice';
 
 const store = configureStore({
   reducer: {
     auth: AuthenticationReducer,
-    room: roomsReducer,
   },
 });
 
