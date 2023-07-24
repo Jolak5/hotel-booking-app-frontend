2e6dbb9a6b59bfe3d0e60fbcaad04de334cd4c35
isaacg <iwanyoike65@gmail.com>
2023-07-24 17:32:53 +0300

    Add details to routes

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -5,40 +5,49 @@ import { fetchHotel } from '../redux/Details/detailsSlice';
 import styles from './Details.module.css';
 
 const Details = () => {
-    const { id } = useParams();
-    const { isLoading, hoteldetails } = useSelector((state) => state.details);
-    const dispatch = useDispatch();
+  const { id } = useParams();
+  const { isLoading, hoteldetails } = useSelector((state) => state.details);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchHotel(id));
-    }, [dispatch, id]);
+  useEffect(() => {
+    dispatch(fetchHotel(id));
+  }, [dispatch, id]);
 
-    if (isLoading) {
-        return (
-            <p>Loading...</p>
-        );
-    }
+  if (isLoading) {
     return (
-        <div className={styles.details}>
-            <div className={styles.divimg}>
-                {hoteldetails.image && <img className={styles.detailsimg} src={hoteldetails.image.url} alt={hoteldetails.name} />}
-            </div>
-            <div className={styles.divdetails}>
-                <h3>{hoteldetails.name}</h3>
-                <table className={styles.detailstable}>
-                    <tr className={styles.divtr}>
-                        <td> Hotel price</td>
-                        <td> {hoteldetails.price} </td>
-                    </tr>
-                    <tr className={styles.divtr}>
-                        <td> Duration </td>
-                        <td> {hoteldetails.duration} </td>
-                    </tr>
-                </table>
-                <button className={styles.reserve}> Reserve </button>
-            </div>
-        </div>
+      <p>Loading...</p>
     );
+  }
+  return (
+    <div className={styles.details}>
+      <div className={styles.divimg}>
+        {hoteldetails.image
+        && <img className={styles.detimg} src={hoteldetails.image.url} alt={hoteldetails.name} />}
+      </div>
+      <div className={styles.divdetails}>
+        <h3>{hoteldetails.name}</h3>
+        <table className={styles.detailstable}>
+          <tr className={styles.divtr}>
+            <td> Hotel price</td>
+            <td>
+              {' '}
+              {hoteldetails.price}
+              {' '}
+            </td>
+          </tr>
+          <tr className={styles.divtr}>
+            <td> Duration </td>
+            <td>
+              {' '}
+              {hoteldetails.duration}
+              {' '}
+            </td>
+          </tr>
+        </table>
+        <button className={styles.reserve} type="submit"> Reserve </button>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
