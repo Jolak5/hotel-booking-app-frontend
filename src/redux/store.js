a4b697e461c0fdfac1ed8106e27022c90132611f
isaacg <iwanyoike65@gmail.com>
2023-07-25 12:59:34 +0300

    add hotel

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import AuthenticationReducer from './auth/authenticationSlice';
 import homeReducer from './Home/homeSlice';
 import detailsReducer from './Details/detailsSlice';
+import newReducer from './newhotel/newhotelSlice';
 
 const store = configureStore({
   reducer: {
     auth: AuthenticationReducer,
     home: homeReducer,
     details: detailsReducer,
+    new: newReducer,
   },
 });
 
