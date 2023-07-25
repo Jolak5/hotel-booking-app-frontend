3f7e862142f933630f78c9692da016f433bd8edc
isaacg <iwanyoike65@gmail.com>
2023-07-25 16:36:12 +0300

    merge hotelroom

--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -2,7 +2,14 @@ export const setLocalStorage = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-export const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+export const getLocalStorage = (key) => {
+  const data = localStorage.getItem(key);
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    return null;
+  }
+};
 
 export const removeLocalStorage = (key) => {
   localStorage.removeItem(key);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import AuthenticationReducer from './auth/authenticationSlice';
+import homeReducer from './Home/homeSlice';
+import detailsReducer from './Details/detailsSlice';
 import ReservationReducer from './reservation/reservation';
 
 const store = configureStore({
   reducer: {
     auth: AuthenticationReducer,
+    home: homeReducer,
+    details: detailsReducer,
     reservations: ReservationReducer,
   },
 });
