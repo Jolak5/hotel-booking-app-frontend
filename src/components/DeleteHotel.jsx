e32a4d174d5116ac50cb36c21abee06a3d7f21b6
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-07-26 15:51:27 +0300

    Fetched hotels from model

--- src/components/DeleteHotel.jsx
+++ src/components/DeleteHotel.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import '../styles/Delete.css';
 
-const DeleteHotel = () => <div>DeleteHotel</div>;
+const DeleteHotel = () => {
+  const { isLoading, hotels } = useSelector((state) => state.home);
+  // const dispatch = useDispatch();
+
+  if (isLoading) {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
+  return (
+    <>
+      <div className="main-del-container">
+        {hotels.map((hotel) => (
+          <div className="delete-container" key={hotel.id}>
+            <img className="delete-image" src={hotel.image.url} alt={hotel.name} />
+            <h3 className="delete-name">{hotel.name}</h3>
+            <Link className="delete-button" to="/">
+              DELETE
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default DeleteHotel;
